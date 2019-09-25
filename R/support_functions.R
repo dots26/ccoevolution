eff773eac3b43491f1fcfae5dbe2e001600a764a
Dani Irawan <irawan_dani@yahoo.com>
2019-09-25 15:35:59 +0200

    Support the vectorized function to also work with cmaes package

--- R/support_functions.R
+++ R/support_functions.R
@@ -1,45 +1,3 @@
-#' Evaluate individual with the specified test function. Non-feasible solution are given Inf as objective values.
-#' @title Evaluate objective values of a single individual
-#' @param individual The individual to be evaluated
-#' @param fun A string containing which problem are being solved.
-#' @param ... Further parameters used by \code{fun}
-#' @return A matrix of size nObjective, containing the objective values.
-#' @examples
-#' individual <- runif(8,1)
-#' EvaluateIndividual(individual,WFG4,3) # the 3 is passed to WFG4 nObj
-#' @export
-EvaluateIndividual <- function(individual,fun,...){
-  nVar <- length(individual)
-
-  objective <- do.call((fun),args = list(individual,...))
-  return(objective)
-}
-
-
-
-#' Evaluate a population with the specified test function. Non-feasible solution are given Inf as objective values.
-#' @title Evaluate objective value of a set of individuals
-#' @param pop The population to be evaluated
-#' @param fun A string containing which problem are being solved.
-#' @param ... Further parameters used by \code{fun}
-#' @return A matrix of size nObjective, containing the objective values.
-#' @examples
-#' pop <- runif(8,50) # 8 variables, 50 individuals
-#' EvaluateIndividual(pop,WFG4,3) # the 3 is passed to WFG4 nObj
-#' @export
-EvaluatePopulation <- function(pop,fun,...){
-  popSize <- ncol(pop)
-  popObjective <- NULL
-#  timest <- Sys.time()
-  popObjective <- foreach::foreach(individualIndex=1:popSize,.combine = 'cbind')%dopar%{
-    indObjective <- EvaluateIndividual(pop[,individualIndex],fun,...)
-  }
-#  timediff<-Sys.time()-timest
-
-  #return(list(pop=popObjective,time=timediff))
-  return(popObjective)
-}
-
 #' Create initial sample using LHS method. The variables will be ranged between 0-1
 #' @title Initialize population with Latin Hypercube Sampling
 #' @param numberOfIndividuals The number of individual in the population. Integer > 0.
@@ -65,8 +23,9 @@ InitializePopulationLHS <- function(numberOfIndividuals,chromosomeLength,minVal=
 }
 
 subfunction <- function(population,contextVector,groupMember,mainfun,...){
-  if(is.matrix(population))
+  if(is.matrix(population)){
     popSize <- nrow(population)
+  }
   if(is.vector(population))
     popSize <- 1
 
@@ -78,4 +37,27 @@ subfunction <- function(population,contextVector,groupMember,mainfun,...){
 
 
   objectiveValue <- mainfun(contextVector,...)
+}
+
+subfunctionCMA <- function(population,contextVector,groupMember,mainfun,...){
+  if(is.matrix(population)){
+    population <- t(population)
+    popSize <- nrow(population)
+  }
+  if(is.vector(population))
+    popSize <- 1
+
+  nVar <- length(contextVector)
+  contextVector <- rep(contextVector,popSize)
+
+  contextVector <- t(matrix(contextVector,nrow=nVar))
+  contextVector[,groupMember] <- population
+
+
+  objectiveValue <- mainfun(contextVector,...)
+}
+
+tfun <- function(x,mainfun,...){
+  res <- mainfun(x,...)
+  return(t(res))
 }
