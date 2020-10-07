7c972482727500619d8ed72d6688f8838fc3cef4
Dani Irawan <irawan@Danis-MacBook-Pro.local>
2020-10-07 11:32:50 +0200

    7 oct update

--- CC_SMS_results/postprocess.R
+++ CC_SMS_results/postprocess.R
@@ -1,14 +1,38 @@
 rm(list=ls())
 meanHV <- NULL
-for(ID in c(2)){
+nIterA <- c(1,250,500,1000)
+
+nPoint <- 1000
+# paretoSetSample <- matrix((randtoolbox::sobol(n = nPoint,dim = k,scrambling = 3))*(1:k)*2,nrow=k)
+# paretoSetSample <- t(matrix((randtoolbox::sobol(k*nPoint))*(1:k)*2,ncol=k))
+# paretoSetSample <- matrix((runif(k*nPoint)^50)*(1:k)*2,nrow=k)
+#paretoSetSample <- matrix((runif(k*nPoint))*(1:k)*2,nrow=k)
+paretoSetSample <- matrix(1:nPoint * (1/nPoint),nrow=1)*(1:k)*2 # wfg2
+aa <- pracma::repmat(rep(0,nPoint),k-1,1)
+paretoSetSample <- rbind(paretoSetSample,aa)
+bb <- pracma::repmat(matrix(0.7*((k+1):nVar),ncol=1),1,nPoint)
+paretoSetSample <- rbind(paretoSetSample,bb)
+paretoFrontSample <- func(individual = paretoSetSample,nObj = 2,k = 20)
+xx <- (0:1000)/1000
+yy <- (1-xx*xx)^0.5
+xx <- paretoFrontSample[1,order(paretoFrontSample[1,])]
+yy <- paretoFrontSample[2,order(paretoFrontSample[1,])]
+
+for (nIter in 250)
+for(ID in c(3)){
   sumHV <- 0
   count <- 0
-  instanceCount <-  length(list.files(path = paste0("global_stepsize/WFG2/test",ID)))
-  for(instance in 1){
-    print(paste(ID,instance))
-    load(paste0("global_stepsize/WFG2/test",ID,"/instance",instance,".Rdata"))
+  # instanceCount <-  length(list.files(path = paste0("global_stepsize/WFG2/test",ID)))
+  for(instance in 1:10){
+    # print(paste(ID,instance))
+    load(paste0("WFG2/fixed_step/",nIter,"iter_ID",ID,"_instance",instance,".Rdata"))
     sumHV <- sumHV + abc$hv_log
+    print(abc$hv_log)
     count <- count + 1
+    plot(xx,yy,xlim=c(0,6),ylim=c(0,8),type="l")
+    points(abc$y)
+    title(paste(ID,instance))
   }
   meanHV <- append(meanHV,sumHV/count)
 }
+View(meanHV)
--- test_SMSEMOA.R
+++ test_SMSEMOA.R
@@ -1,24 +1,27 @@
 rm(list=ls())
 library(MaOEA)
-setwd("~/Desktop/MOLSGO/ccoevolution")
+qq <- 9
+#setwd("~/Desktop/MOLSGO/ccoevolution")
 files.sources = list.files(path = "R",full.names = T,pattern = ".R")
 sapply(files.sources, source)
-# source('local_mutation_rateSMSEMOA.R')  
+source('local_mutation_gaussSMSEMOA.R')
 set.seed(100)
 nIterInner <- c(1,250,500,1000)
-func <- MaOEA::WFG2
+chosennIter <- (qq-1)%%4+1
+nIterInner <- nIterInner[chosennIter]
+func <- MaOEA::WFG4
 adapt_step_log <- list()
-# formals(func) <- alist(individual=nObj=m,k= nObj-1)
+
 m <- 2
 evalInterval <- 1e5
 vargroup <- vector(mode = 'list',length = m)
 a <- vector(mode = 'list',length = m)
-r <- 1
-nVar <- 100
+r <- 4
+nVar <- 1000
 lbound <- rep(0,nVar)
 ubound <- (1:nVar)*2
 nEval <- 0
-k <- 20
+k <- 250
 vars <- 1:nVar
 
 transposedFun <- function(x,mainfunc,...){
@@ -42,15 +45,11 @@ morrisMultOut <- function (model = NULL, factors, r = r,
 timelog <- NULL
 abc_log <- vector('list')
 
-for (testID in c(2:7))
+for (testID in (qq-1)%/%4+1)
 {
-  print(paste("TestID",testID))
-  if(testID>13){
-    testIDinUse <- testID-13
-  }else{
-    testIDinUse <- testID
-  }
-  nLevel <- c(1,50,10,4,50,10,4,50,10,4,50,10,4)
+  testIDinUse <- testID+1
+  
+  nLevel <- c(1,50,10,4,50,10,4,50,10,4,50,10,4,50,10,4,50,10,4)
   nLevel <- nLevel[testIDinUse]
   budget <- 10000
   popSize <- 100
@@ -77,18 +76,6 @@ for (testID in c(2:7))
   samples <- sample(1:nrow(SA$X),popSize)
   x <- SA$X[samples,]
   objs <- SA$objs[samples,]
-  rnk <- nsga2R::fastNonDominatedSorting(objs)
-  #print('non_domsort done')
-  rnkIndex <- integer(ncol(objs))
-  sortingIndex <- 1;
-  while (sortingIndex <= length(rnk)) {
-    rnkIndex[rnk[[sortingIndex]]] <- sortingIndex;
-    sortingIndex <- sortingIndex + 1;
-  }
-  print('sorting done')
-  # fill new population
-  
-  print('population built')
   
   reg_coefficient <- vector('list')
   group <- NULL
@@ -98,7 +85,6 @@ for (testID in c(2:7))
   ranks <- order(mu.star,decreasing = T)
   
   reg_coefficient <- matrix(mu.star)
-  # for(i in 1:m){
   totalCoef <- sum(reg_coefficient)
   
   rankedVars <- vars[ranks]
@@ -121,28 +107,69 @@ for (testID in c(2:7))
   CV <- x
   CVobjs <- objs
   
-  ctrl <- list(group = group,importance=reg_coefficient,
-               lower = lbound,upper = ubound,
-               budget=budget-nEval,refmultiplier=2,
-               stepadapt=F,
-               #budget = budget,
-               vectorized=T, scaleinput=F,loghv=F,refpoint=c(2.2,4.4))
-
   base <- F
-  for(i in 1:10){
+  if(testIDinUse==1)
+    base <- T
+  
+  for(i in 1:1){
     set.seed(i*100)
     start_1 <- Sys.time()
     gs <- c("",rep(c("roundrobin","roundrobin","roundrobin",
-            "adaptive","adaptive","adaptive"),2))
+                     "adaptive","adaptive","adaptive"),3))
     
     gs <- gs[testIDinUse]
+    
+    # group <- list((1:20),(21:40),(41:60),(61:80),(81:100))
+    
+    population <- NULL
+    for(groupingIndex in 1:length(group)){
+      thisnVar <- length(group[[groupingIndex]])
+      member <- group[[groupingIndex]]
+      population_group <- t(InitializePopulationSobol(ncol = 98,
+                                                    nrow = thisnVar,
+                                                    minVal = lbound[member],
+                                                    maxVal = ubound[member]))
+      # population_group <- rbind(population_group,(lbound[]))
+      newpop <- lbound[member]
+      newpop[1:10] <- ubound[member[1:10]]
+      population_group <- rbind(population_group,newpop)
+
+      
+      newpop <- lbound[member]
+      newpop[11:20] <- ubound[member[11:20]]
+      population_group <- rbind(population_group,newpop)
+      
+      population <- cbind(population,population_group)
+    }
+    for(i in 1:100)population[i,21:100] <- (21:100)*2*0.35
+    # group <- list((1:20))
+    
+    # population <- MaOEA::InitializePopulationLHS(100,nVar,lbound,ubound)
+    # population <- matrix(runif(100*nVar),nrow=100)*2*(1:100)
+    # for(i in 1:100)population[21:100,i] <- (21:100)*2*0.35
+    # population = t(population)
+    # 
+    # population <- NULL
+    
+    ctrl <- list(group = group,importance=reg_coefficient,
+                 lower = lbound,upper = ubound,
+                 budget=budget-nEval,refmultiplier=2,
+                 stepadapt=T,
+                 crossoverrate=0.5,
+                 mutationrate=0.05,
+                 stepsize=0.3,
+                 alpha=1.2,
+                 crossoveroperator="uniform",
+                 mutationoperator="truncgauss",
+                 localmutation=T,
+                 vectorized=T, scaleinput=F,loghv=F,refpoint=c(2.2,4.4))
     if(!base){
-      abc <- CC_SMSEMOA( population = NULL, nVar = nVar, populationSize  = 100,
-                         fun = transposedFun, mainfunc=func, k=k, nObj =2,growPop=T,
+      abc <- CC_SMSEMOA( population = population, nVar = nVar, populationSize  = 100,
+                         fun = transposedFun, mainfunc=func, k=k, nObj =2,growPop=F,
                          maxPopulationSize=100,nIterInner=nIterInner,
                          control=ctrl,group_selection=gs)
     }else{
-      population <- MaOEA::InitializePopulationLHS(100,1000,lbound,ubound)
+      population <- MaOEA::InitializePopulationLHS(100,nVar,lbound,ubound)
       for(i in 1:budget){
         print( paste("progress:",100*i/budget) )
         start_2 <- Sys.time()
@@ -166,11 +193,14 @@ for (testID in c(2:7))
       }
     }
     end_1 <- Sys.time()
-    abc_log <- append(abc_log,abc)
+    #abc_log <- append(abc_log,abc)
     timelog <- append(timelog,as.numeric(end_1-start_1))
     load("adapt_step.Rdata")
-    adapt_step_log <- append(adapt_step_log,list(cdlog,mdlog))
-    save(list=ls(),file=paste0("global_stepsize_adaptation/",nIterInner,'iter_ID',testID,'_instance',i,'.Rdata'))
+    adapt_step_log <- append(adapt_step_log,list(cdlog=cdlog,
+                                                 mdlog=mdlog,
+                                                 sec_steplog=altstep_log,
+                                                 stepsizelog=stepsize_log))
+    save(list=ls(),file=paste0("checkgauss",i,".Rdata"))
   }
 }
 
@@ -187,8 +217,7 @@ for (testID in c(2:7))
 # paretoFrontSample <- func(individual = paretoSetSample,nObj = 2,k = 20)
 # xx <- (0:1000)/1000
 # yy <- (1-xx*xx)^0.5
-# xx <- paretoFrontSample[1,order(paretoFrontSample[1,])]/2
-# yy <- paretoFrontSample[2,order(paretoFrontSample[1,])]/4
+# xx <- paretoFrontSample[1,order(paretoFrontSample[1,])]
+# yy <- paretoFrontSample[2,order(paretoFrontSample[1,])]
 # plot(xx,yy,type="l",xlim= c(0,2),ylim=c(0,2))
 # points(t(t(abc$y)/c(2,4)),col='red')
- 
